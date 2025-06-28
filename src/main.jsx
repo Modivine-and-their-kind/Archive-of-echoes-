b0d5c27b358bd3dfe45340afb57621e2dde2d1b2
Modivine-and-their-kind <sverojonero@gmail.com>
2025-06-28 05:57:56 +0200

    ey
    yooo

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ArchiveOfEchoes from "./pages/ArchiveOfEchoes";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ArchiveOfEchoes />
+  </React.StrictMode>
+);
